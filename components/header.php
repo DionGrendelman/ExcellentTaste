7949d1b69bdb69e8847bb16f04043a15db18e224
Dion <d.grendelman01@student.alfa-college.nl>
2019-05-20 10:14:51 +0200

    added file structure

---
+++ components/header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!--Require META"s-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Require Links-->
+    <link rel="stylesheet" href="/assets/css/style.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+          integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <title>Excellent Taste</title>
+</head>
+<body>
+<div class="wrap">
+    <div class="title">
+        <h2>Excellent Taste</h2>
+    </div>
+    <nav>
+        <ul id="menu">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/bestellingen">Bestellingen</a>
+                <ul class="submenu">
+                    <li><a href="/bestellingen/bon">Bon voor klant</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="/reserveringen">Reserveringen</a>
+            </li>
+            <li>
+                <a href="/overzichten">Overzichten</a>
+                <ul class="submenu">
+                    <li><a href="/overzichten/barman">Overzicht barman</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div class="content">

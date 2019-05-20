cfc8fe5dd7fc41a08fe916a639caf9608730d1f1
Dion <d.grendelman01@student.alfa-college.nl>
2019-05-20 09:12:34 +0200

    file structure

--- assets/js/overzichten/barman_bestelling.js
+++
@@ -1,6 +0,0 @@
-$('#bon_selector').on('change', function () {
-    var date = $('option:selected').data('date');
-    var time = $('option:selected').data('time')
-    var table = $('option:selected').data('table')
-    alert($('option:selected').data('date'));
-});
--- assets/js/reserveringen/main.js
+++
@@ -1,6 +0,0 @@
-$('#loadReserveringen').load('/controllers/tables/reserveringen/loadReservering.php', function () {
-});
-
-$('#newReservering').on('click',function () {
-    window.location.href = '/reserveringen/nieuw';
-});

c5b7ba5a0f82593b7c23c3bfd9f2479f5575939a
Dion <d.grendelman01@student.alfa-college.nl>
2019-05-23 10:30:26 +0200

    Changed colors and font

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/libraries/Medoo.php';
 
 define("DB_INIT",  array(
         'database_type' => 'mysql',
-        'database_name' => 'excellent_db',
+        'database_name' => 'excellent_dbmain',
         'server' => 'localhost',
         'username' => 'root',
         'password' => ''
